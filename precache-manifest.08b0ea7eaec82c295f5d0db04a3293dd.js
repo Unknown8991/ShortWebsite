dfdec6a8c14260ac3490f4b4359182c63033d481
Unknown8991 <rafal.pat@onet.pl>
2021-09-13 21:04:44 +0200

    Updates

---
+++ precache-manifest.08b0ea7eaec82c295f5d0db04a3293dd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d1688a070f490954d5a",
+    "url": "/ShortWebsite/static/css/main.e2e36fdd.chunk.css"
+  },
+  {
+    "revision": "5d1688a070f490954d5a",
+    "url": "/ShortWebsite/static/js/main.002528a0.chunk.js"
+  },
+  {
+    "revision": "a590482a7ee2e620487a",
+    "url": "/ShortWebsite/static/js/runtime~main.2a66a91e.js"
+  },
+  {
+    "revision": "434a90d08d6962535129",
+    "url": "/ShortWebsite/static/css/2.4121947d.chunk.css"
+  },
+  {
+    "revision": "434a90d08d6962535129",
+    "url": "/ShortWebsite/static/js/2.ea3d9b5f.chunk.js"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/ShortWebsite/static/media/react.f297dfcd.png"
+  },
+  {
+    "revision": "7334348ad6f656dffe78adc0d45ced53",
+    "url": "/ShortWebsite/static/media/html-5.7334348a.svg"
+  },
+  {
+    "revision": "2db97d69ba44f8da4663584e8197143f",
+    "url": "/ShortWebsite/static/media/css-3.2db97d69.svg"
+  },
+  {
+    "revision": "3023657df2a80a0f8957a339d14ef515",
+    "url": "/ShortWebsite/static/media/sass.3023657d.svg"
+  },
+  {
+    "revision": "e169ba293cabaff0dc96397aed47a250",
+    "url": "/ShortWebsite/static/media/javascript.e169ba29.svg"
+  },
+  {
+    "revision": "b00020dde0490b3f0ecb3643f369d99c",
+    "url": "/ShortWebsite/static/media/icons8-vue-js.b00020dd.svg"
+  },
+  {
+    "revision": "1f33e75d7b7df995b1609f8573ac8c9b",
+    "url": "/ShortWebsite/static/media/github.1f33e75d.svg"
+  },
+  {
+    "revision": "5b23a837268b09d0efa9e307023c1dae",
+    "url": "/ShortWebsite/static/media/instagram.5b23a837.svg"
+  },
+  {
+    "revision": "47db48748fa917217b9d1ec6d9f9a087",
+    "url": "/ShortWebsite/index.html"
+  }
+];
